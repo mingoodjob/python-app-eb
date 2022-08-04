b3d7d35cb087f14c6e0749a18d33d6271ac114f9
이민기 <psjlmk@gmail.com>
2022-08-04 18:20:54 +0900

    fix

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ def file_upload():
                       )
     s3.put_object(
         ACL="public-read",
-        Bucket="pracs3",
+        Bucket=os.environ["BUCKET_NAME"],
         Body=file,
         Key=file.filename,
         ContentType=file.content_type
